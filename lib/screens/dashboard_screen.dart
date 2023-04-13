0a5f875ea14fd01a43fc9dfa2922aca8c5862602
marceyuli <yulianamarcela200@gmail.com>
2023-04-13 13:57:51 -0400

    Upper widgets in the dashboard screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart' hide MenuController;
 import 'package:grocery_admin_panel/consts/constants.dart';
 import 'package:grocery_admin_panel/responsive.dart';
 import 'package:grocery_admin_panel/services/utils.dart';
+import 'package:grocery_admin_panel/widgets/buttons.dart';
 import 'package:grocery_admin_panel/widgets/grid_products.dart';
 import 'package:grocery_admin_panel/widgets/header.dart';
 import 'package:grocery_admin_panel/widgets/orders_list.dart';
 import 'package:grocery_admin_panel/widgets/products_widget.dart';
+import 'package:grocery_admin_panel/widgets/text_widget.dart';
 import 'package:provider/provider.dart';
 
 import '../controllers/MenuController.dart';
@@ -15,6 +17,7 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final color = Utils(context).color;
     Size size = Utils(context).getScreenSize;
     return SafeArea(
       child: SingleChildScrollView(
@@ -27,7 +30,29 @@ class DashboardScreen extends StatelessWidget {
                 context.read<MenuController>().controlDashboarkMenu();
               },
             ),
-            const SizedBox(height: defaultPadding),
+            TextWidget(text: 'Latest Products', color: color),
+            const SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  ButtonsWidget(
+                    onPressed: () {},
+                    text: 'View all',
+                    icon: Icons.store,
+                    backgroundColor: Colors.blue,
+                  ),
+                  Spacer(),
+                  ButtonsWidget(onPressed: (){}, text: 'Add product', icon: Icons.add, backgroundColor: Colors.blue)
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const SizedBox(
+              height: defaultPadding,
+            ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -39,10 +64,12 @@ class DashboardScreen extends StatelessWidget {
                         mobile: ProductsGrid(
                           crossAxisCount: size.width < 650 ? 2 : 4,
                           childAspectRatio:
-                              size.width < 650 && size.width > 350 ? 1.1 : 0.8, isInMain: true,
+                              size.width < 650 && size.width > 350 ? 1.1 : 0.8,
+                          isInMain: true,
                         ),
                         desktop: ProductsGrid(
-                          childAspectRatio: size.width < 1400 ? 0.8 : 1.05, isInMain: true,
+                          childAspectRatio: size.width < 1400 ? 0.8 : 1.05,
+                          isInMain: true,
                         ),
                       ),
                       const OrdersList(),
